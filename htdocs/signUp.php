ca5dfb1921e8bae4c5051864761e9230f480e37e
wdf589 <33269677+wdf589@users.noreply.github.com>
2018-11-23 11:36:36 +0900

    Update signUp.php

--- htdocs/signUp.php
+++ htdocs/signUp.php
@@ -1,6 +1,14 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        
+        
+        <style>
+
+
+
+        </style>
+        
         <link rel= "stylesheet" type="text/css" href="/signUp.css">
         <title>
             회원가입
@@ -11,48 +19,73 @@
     
     <body>
         <div class="wrap">
-           <header>회원 가입</header>
+           <header><img src="/picture/index_img/logo.png" alt="로고"></header>
+           
+           <table class="bar">
+               <tr>
+                   <td><span>문제 풀이</span></td>
+                   <td><span>문제 풀이</span></td>
+                   <td><span>문제 풀이</span></td>
+                   <td><span id="last">문제 풀이</span></td>
+               </tr>
+           </table>
+           
             <form name="data" method="post" onsubmit="return Login()">
-                <div class="login">
-                    <div class="id_wrap">
-                        <span class="id">아이디</span>: <input type="text" name="id" id="id" maxlength="12" onKeyup=id_check(this)>
+                <div class="login_wrap">
+                    <div class="login">
+                       <span id="login_title">회원가입</span>
+                        <div class="id_wrap">
+                            <span class="id">아이디</span>
+                            
+                            <input type="text" name="id" id="id" maxlength="12" onKeyup=id_check(this)>
+                        </div>
+                        
+                        <span id="id_check"></span>
+                        <br>
+                        <div class="password_wrap">
+                            <span class="password">비밀번호</span>
+                            
+                            <input type="password" name="password" id="password" maxlength="12" onKeyup=password_check(this) >
+                        </div>
+                        
+                        <span id="password_check"></span>
+                        <br>
+                        <div class="password_confirm_wrap">
+                            <span class="password_confirm">비밀번호 확인</span>
+                            
+                            <input type="password" name="password_confirm" id="password_confirm" maxlength="12" onKeyup=password_check(this)>
+                        </div>
+                        
+                        <span id="password_confirm_check"></span>
+                        <br>
+                        <div class="mail_wrap">
+                            <span class="mail">이메일</span>
+                            
+                            <input type="text" name="mail" id="mail" onKeyup=mail_check(this)>
+                        </div>
+                        
+                        <span id="mail_check"></span>
+                        <br>
+                        <div class="name_wrap">
+                            <span class="name">이름</span>
+                            
+                            <input type="text" name="name" id="name" maxlength="12" onKeyup=name_check(this)>
+                        </div>
+                        
+                        <span id="name_check"></span>
+                        <br>
+                        <div class="nick_wrap">
+                            <span class="nick">닉네임</span>
+                            
+                            <input type="text" name="nick" id="nick" maxlength="12" onKeyup=nick_check(this)>
+                        </div>
+                        
+                        <span id="nick_check"></span>
+                        <br>
+                        <p>
+                            <input type="submit" value="회원가입">
+                        </p>
                     </div>
-                    
-                    <span id="id_check"></span>
-                    <br>
-                    <div class="password_wrap">
-                        <span class="password">비밀번호</span>: <input type="password" name="password" id="password" maxlength="12" onKeyup=password_check(this) >
-                    </div>
-                    
-                    <span id="password_check"></span>
-                    <br>
-                    <div class="password_confirm_wrap">
-                        <span class="password_confirm">비밀번호 확인</span>: <input type="password" name="password_confirm" id="password_confirm" maxlength="12" onKeyup=password_check(this)>
-                    </div>
-                    
-                    <span id="password_confirm_check"></span>
-                    <br>
-                    <div class="mail_wrap">
-                        <span class="mail">이메일</span>: <input type="text" name="mail" id="mail" onKeyup=mail_check(this)>
-                    </div>
-                    
-                    <span id="mail_check"></span>
-                    <br>
-                    <div class="name_wrap">
-                        <span class="name">이름</span>: <input type="text" name="name" id="name" maxlength="12" onKeyup=name_check(this)>
-                    </div>
-                    
-                    <span id="name_check"></span>
-                    <br>
-                    <div class="nick_wrap">
-                        <span class="nick">닉네임</span>: <input type="text" name="nick" id="nick" maxlength="12" onKeyup=nick_check(this)>
-                    </div>
-                    
-                    <span id="nick_check"></span>
-                    <br>
-                    <p>
-                        <input type="submit" value="확인">
-                    </p>
                 </div>
                 
             </form>
