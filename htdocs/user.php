406614bcb3a70bd2cb626df379c6e35069993418
wdf589 <33269677+wdf589@users.noreply.github.com>
2018-11-24 22:28:55 +0900

    Update user.php

--- htdocs/user.php
+++ htdocs/user.php
@@ -30,18 +30,36 @@ if(isset($_POST['change'])){
                 echo $_SESSION['nick']."님의 회원정보";
             ?>
     </title>
+    
 </head>
 <body>
-    <form method="post" target="_self">
+    <form method="post" target="_self" onsubmit="return Login()">
     <?php
         echo $_SESSION['nick']."님의 회원정보";
-        echo "아이디: <input type=\"text\" name=\"id\" value=\"".$_SESSION['id']."\">";
-        echo "현재 비밀번호: <input type=\"password\" name=\"pw\">";
-        echo "새로운 비밀번호: <input type=\"password\" name=\"npw\">";
-        echo "비밀번호 확인: <input type=\"password\" name=\"npw_re\">";
-        echo "이름: <input type=\"text\" name=\"name\" value=\"".$_SESSION['name']."\">";
-        echo "이메일: <input type=\"text\" name=\"email\" value=\"".$_SESSION['email']."\">";
-        echo "닉네임: <input type=\"text\" name=\"nick\" value=\"".$_SESSION['nick']."\">";
+        echo "<br>";
+        echo "<span class=\"id\">아이디</span>: <input type=\"text\" id=\"id\" name=\"id\" value=\"".$_SESSION['id']."\" maxlength=\"12\" onKeyup=id_check(this)>";
+        echo "<span id=\"id_check\"></span>";
+        echo "<br>";
+        echo "<span class=\"pw\">비밀번호</span>: <input type=\"password\" id=\"pw\" name=\"pw\" maxlength=\"12\" onKeyup=password_check(this)>";
+        echo "<span id=\"pw_check\"></span>";
+        echo "<br>";
+        echo "<span class=\"npw\">새로운 비밀번호</span>: <input type=\"password\" id=\"npw\" 
+        name=\"npw\" maxlength=\"12\" onKeyup=password_check(this)>";
+        echo "<span id=\"npw_check\"></span>";
+        echo "<br>";
+        echo "<span class=\"password_confirm\">비밀번호 확인</span>: <input type=\"password\" id=\"npw_re\" name=\"npw_re\" maxlength=\"12\" onKeyup=password_check(this)> ";
+        echo "<span id=\"password_confirm_check\"></span>";
+        echo "<br>";
+        echo "<span class=\"name\">이름</span>: <input type=\"text\" id=\"name\" name=\"name\" 
+        value=\"".$_SESSION['name']."\" maxlength=\"12\" onKeyup=name_check(this)>";
+        echo "<span id=\"name_check\"></span>";
+        echo "<br>";
+        echo "<span class=\"mail\">이메일</span>: <input type=\"text\" id=\"email\" name=\"email\" value=\"".$_SESSION['email']."\" onKeyup=mail_check(this)>";
+        echo "<span id=\"mail_check\"></span>";
+        echo "<br>";
+        echo "<span class=\"nick\">닉네임</span>: <input type=\"text\" id=\"nick\" name=\"nick\" value=\"".$_SESSION['nick']."\" onKeyup=nick_check(this)>";
+        echo "<span id=\"nick_check\"></span>";
+        echo "<br>";
         echo "<input type=\"submit\" name=\"change\" value=\"수정\">";
     ?>
     </form>
