d2a5332144a0bb47c45782534557e556414e7244
wdf589 <33269677+wdf589@users.noreply.github.com>
2018-11-25 18:30:09 +0900

    Update user.js

--- htdocs/user.js
+++ htdocs/user.js
@@ -1,77 +1,58 @@
-        function id_check(obj) {
-            var getCheck = new RegExp(/^[a-zA-Z0-9]{4,12}$/);
+function id_check(obj){
+            var getCheck= RegExp(/^[a-zA-Z0-9]{6,12}$/);
             
-            if(obj.value.length > 0 && obj.value.length <= 3){
-                $("#id_check").html("아이디를 4자리 이상으로 입력해 주십시오.");
-                $(".id").css({'color' : 'red'});
+            if(obj.value.length>0 && obj.value.length<=5){
+                $("#id_check").html("(일치하지 않습니다.)");
+                $("#id_check").css({'color':'red'});
             }
-            if(obj.value.length>=4){
-                $("#id_check").html("");
+            if(obj.value.length>=6){
                 if(!getCheck.test($("#id").val())){
-                    $("#id_check").html("id는 영어 대소문자 혹은 숫자로만 이루어져야 합니다.");
-                    $(".id").css({'color':'red'});
+                    $("#id_check").html("(일치하지 않습니다.)");
+                    $("#id_check").css({'color':'red'});
                 }
             }
             if(getCheck.test($("#id").val())){
                 $("#id_check").html("");
-                $(".id").css({'color':'green'});
+                $("#id_check").css({'color':'green'});
             }
 
         }
         
         function password_check(obj){
-            var pw = $("#npw").val();
-            var pwc = $("#npw_re").val();
-            var ori_pw = $("#pw").val();
+            var pw=$("#pw").val();
+            var pwc=$("#npw").val();
             
-            var getCheck= RegExp(/^[a-zA-Z0-9]{4,12}$/);
-            
-            if(ori_pw.length>0 && ori_pw.length<=3){
-                $("#pw_check").html("비밀번호를 4자리 이상으로 입력해 주십시오.");
-                $(".pw").css({'color':'red'});
+            var getCheck= RegExp(/^[a-zA-Z0-9]{8,12}$/);
+            if(obj.value.length>0 && obj.value.length<=7){
+                $("#pw_check").html("(일치하지 않습니다.)");
+                $("#pw_check").css({'color':'red'});
             }
-            if(ori_pw.length>=4){
+            if(obj.value.length>=8){
                 if(!getCheck.test($("#pw").val())){
-                    $("#pw_check").html("비밀번호는 영어 대소문자 혹은 숫자로만 이루어져야 합니다.");
-                    $(".pw").css({'color':'red'});
-                }
-            }
-            
-            if(pw.length>0 && pw.length<=3){
-                $("#npw_check").html("비밀번호를 4자리 이상으로 입력해 주십시오.");
-                $(".npw").css({'color':'red'});
-            }
-            if(pw.length>=4){
-                if(!getCheck.test($("#npw").val())){
-                    $("#npw_check").html("비밀번호는 영어 대소문자 혹은 숫자로만 이루어져야 합니다.");
-                    $(".npw").css({'color':'red'});
+                    $("#pw_check").html("(일치하지 않습니다.)");
+                    $("#pw_check").css({'color':'red'});
                 }
             }
             
             if(pw == "" && pwc != ""){
-                $("#password_confirm_check").html("비밀번호를 먼저 입력해 주십시오.");
-                $(".password_confirm").css({'color':'red'});
+                $("#npw_check").html("비밀번호를 먼저 입력해 주십시오.");
+                $("#npw_check").css({'color':'red'});
             }
             
             if(pw != "" && pwc != ""){
                 if(pw==pwc){
-                    $("#password_confirm_check").html("");
-                    $(".password_confirm").css({'color':'green'});
+                    $("#npw_check").html("");
+                    $("#npw_check").css({'color':'green'});
                 }
                 else{
-                    $("#password_confirm_check").html("비밀번호가 다릅니다.");
-                    $(".password_confirm").css({'color':'red'});
+                    $("#npw_check").html("다시 한번 확인해주세요.");
+                    $("#npw_check").css({'color':'red'});
                 }
-            }
-            
-            if(getCheck.test($("#npw").val())){
-                $("#npw_check").html("");
-                $(".npw").css({'color':'green'});
             }
             
             if(getCheck.test($("#pw").val())){
                 $("#pw_check").html("");
-                $(".pw").css({'color':'green'});
+                $("#pw_check").css({'color':'green'});
             }
             
 
@@ -81,56 +62,24 @@
             var getMail = RegExp(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/);
             if(obj.value.length>0){
                 if(!getMail.test($("#email").val())){
-                    $("#mail_check").html("mail형식에 맞게 입력해주십시오.");
-                    $(".mail").css({'color':'red'});
+                    $("#mail_check").css({'color':'red'});
                 }
             }
             
             if(getMail.test($("#email").val())){
-                $("#mail_check").html("");
-                $(".mail").css({'color':'green'});
+                $("#mail_check").css({'color':'green'});
             }
 
         }
         
-        function name_check(obj){
-            var getName= RegExp(/^[가-힣]+$/);
-            
-            if(obj.value.length>0){
-                if(!getName.test($("#name").val())){
-                    $("#name_check").html("올바른 한글로만 입력해주십시오.");
-                    $(".name").css({'color':'red'});
-                }
-            }
-            
-            if(getName.test($("#name").val())){
-                $("#name_check").html("");
-                $(".name").css({'color':'green'});
-            }
-        }
+    function Login(){
+      var getMail = RegExp(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/);
+      var getCheck= RegExp(/^[a-zA-Z0-9]{4,12}$/);
+      var getName= RegExp(/^[가-힣]+$/);
         
-        function nick_check(obj){
-            var getCheck= RegExp(/^[a-zA-Z0-9]{1,12}$/);
-            
-            if(obj.value.length>0){
-                if(!getCheck.test($("#nick").val())){
-                    $("#nick_check").html("영어 대소문자와 숫자로만 입력해주십시오.");
-                    $(".nick").css({'color':'red'});
-                }
-            }
-            
-            if(getCheck.test($("#nick").val())){
-                $("#nick_check").html("");
-                $(".nick").css({'color':'green'});
-            }
-            
-
-        }
-        
-        function Login(){
-            var getMail = RegExp(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/);
-            var getCheck= RegExp(/^[a-zA-Z0-9]{4,12}$/);
-            var getName= RegExp(/^[가-힣]+$/);
+      $("#id").on('keydown', function(){
+          $('h1').html(" good");
+      })
         
       if($("#id").val() == ""){
         alert("아이디를 입력해 주십시오");
@@ -146,29 +95,29 @@
       }
 
       
-      if($("#npw").val() == ""){
-        alert("새로운 패스워드를 입력해 주십시오");
-        $("#npw").focus();
+      if($("#pw").val() == ""){
+        alert("패스워드를 입력해 주십시오");
+        $("#pw").focus();
         return false;
       }
 
-      if(!getCheck.test($("#npw").val())){
-        alert("새로운 패스워드를 형식에 맞게 입력해 주십시오");
-        $("#npw").val("");
-        $("#npw").focus();
+      if(!getCheck.test($("#pw").val())){
+        alert("패스워드를 형식에 맞게 입력해 주십시오");
+        $("#pw").val("");
+        $("#pw").focus();
         return false;
       }
       
-      if($("#npw_re").val() == ""){
+      if($("#npw").val() == ""){
         alert("패스워드 확인을 입력해 주십시오");
-        $("#npw_re").focus();
+        $("#pw").focus();
         return false;
       }
         
-      if($("#npw").val() != $("#npw_re").val()){
-        alert("새로운 패스워드와 일치하지 않습니다");
-        $("#npw_re").val("");
-        $("#npw_re").focus();
+      if($("#pw").val() != $("#npw").val()){
+        alert("패스워드와 다릅니다.");
+        $("#npw").val("");
+        $("#npw").focus();
         return false;
       }
 
@@ -183,21 +132,7 @@
         $("#email").val("");
         $("#email").focus();
         return false;
-      }
-
-      if($("#name").val() == ""){
-        alert("이름을 입력해 주십시오");
-        $("#name").focus();
-        return false;
-      }
-        
-      if(!getName.test($("#name").val())){
-        alert("이름을 형식에 맞게 입력해 주십시오")
-        $("#name").val("");
-        $("#name").focus();
-        return false;
-      }
-      
+      } 
       if($("#nick").val() == ""){
         alert("닉네임을 입력해 주십시오");
         $("#nick").focus();
