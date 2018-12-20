4431018839a31bc494627ee7e5c56f45a12f8a3c
Box5789 <pck279049@gmail.com>
2018-12-20 20:46:00 +0900

    Add files via upload

--- htdocs/top.php
+++ htdocs/top.php
@@ -4,5 +4,5 @@
         <img src="/picture/main/logo.png" class="logo" width="235px" height="55px">
     </a>
     <img src="/picture/main/line.png" class="line" width="48px" height="41px">
-    <a href="/help.php"><span class="help">도움말</span></a>
+    <a href="/help.php"><img src="/picture/help/help_1.png" height="79px" width="auto" class="help"></a>
 </div>
